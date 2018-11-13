4193d8eb32d996af9b01223b5114e015a7045a81
Juan Ignacio Avelar Miranda <38658482+juanignacioavelar@users.noreply.github.com>
2018-11-13 11:43:34 -0600

    Update main.c

--- Practica_08/main.c
+++ Practica_08/main.c
@@ -17,21 +17,20 @@
 //----------------------------------- Frecuencia del CPU --------------------------------------
 #define F_CPU 16000000UL			// El AVR trabajara a una velocidad de Reloj de 16MHz.
 
-//----------------------------------- Librerías -----------------------------------------------
-#include <avr/io.h>					// Librería colocada automaticamente por el wizard de Atmel Studio.
-#include <util/delay.h>				// Librería para retardo.
+//----------------------------------- LibrerÃ­as -----------------------------------------------
+#include <avr/io.h>				// LibrerÃ­a colocada automaticamente por el wizard de Atmel Studio.
+#include <util/delay.h>				// LibrerÃ­a para retardo.
 
 //=================================== Programa Principal ======================================
-int main(void)						// Función Principal.
+int main(void)					// FunciÃ³n Principal.
 {
-	DDRB = 0b11111111;				// Configura los 8 pines como salida del puerto B.
-	PORTB = 0b00000000;				// Limpiamos el puerto B.
-    while (1)						// Ciclo while infinito.
-    {
-		PORTB = 0b00100000;			// Mandamos un 1 en el pin PB5, los demas pines del puerto con 0
-		_delay_ms(500);				// Retardo
-		PORTB = 0b00000000;			// Mandamos un 0 cada uno de los pines del puerto B
-		_delay_ms(500);				// Retardo
+	DDRB = 0b11111111;			// Configura los 8 pines como salida del puerto B.
+	PORTB = 0b00000000;			// Limpiamos el puerto B.
+	while (1)				// Ciclo while infinito.
+    	{
+		PORTB = 0b00100000;		// Mandamos un 1 en el pin PB5, los demas pines del puerto con 0
+		_delay_ms(500);			// Retardo
+		PORTB = 0b00000000;		// Mandamos un 0 cada uno de los pines del puerto B
+		_delay_ms(500);			// Retardo
     }
 }
-
